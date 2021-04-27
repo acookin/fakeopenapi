8c118d0bcb3c865b0bc375ca7a40d4f04dcc6ba5
Alix Cook <alixcook@datawire.io>
2021-04-27 15:33:14 -0400

    genopenapi ish

--- gen_openapi.sh
+++ gen_openapi.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
+COUNT=5
+
+if [[ -n "${1}" ]] ; then
+    COUNT=${1}
+fi
 
 X=0
 
-while [ "$X" -lt 3 ] ; do
+while [ "$X" -lt $COUNT ] ; do
     path=`randword`
     name=`randword`
     helm template --set prefix=${path} --set name=${name} ./chart | kubectl apply -f -
